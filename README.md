34464d049156faa6fef6897275cf4c8dc3c863b5
EntryPointKR <entrypoint_@naver.com>
2019-07-19 17:45:54 +0900

    Add get_version_data(), Update README.md

--- README.md
+++ README.md
@@ -5,6 +5,42 @@ ProtocolSupport plugin API for CommandHelper
 
 ## get_protocol_version
 
-Arg: [player]
+Arg: [player|protocolname]
 
-Returns a Protocol Version.
+Returns a protocol version string.
+
+Exmaples
+
+For user: `get_protocol_version('EntryPoint')` 
+
+For protocol name: `get_protocol_version('MINECRAFT_1_14_3')`
+
+## get_protocol_data
+
+Arg: [player|protocolname]
+
+Returns a protocol data array. There is keys id, order, name, type
+
+Examples
+
+For user: `get_protocol_data('EntryPoint')` 
+
+For protocol name: `get_protocol_data('MINECRAFT_1_14_3')`
+
+Version limit example
+
+```javascript
+@order17 = get_protocol_data('MINECRAFT_1_7_10')[order]
+bind(player_join, null, null, @e, @order17) {
+    @playerOrder = get_protocol_data(@e[player])[order]
+    if (@playerOrder <= @order17) {
+        pkick(@e[player], 'Outdated client!')
+    }
+}
+```
+
+# Protocol Names
+
+MINECRAFT_FUTURE, MINECRAFT_1_14_3, MINECRAFT_1_14_2, MINECRAFT_1_14_1, MINECRAFT_1_14, MINECRAFT_1_13_2, MINECRAFT_1_13_1, MINECRAFT_1_13, MINECRAFT_1_12_2, MINECRAFT_1_12_1, MINECRAFT_1_12, MINECRAFT_1_11_1, MINECRAFT_1_11, MINECRAFT_1_10, MINECRAFT_1_9_4, MINECRAFT_1_9_2, MINECRAFT_1_9_1, MINECRAFT_1_9, MINECRAFT_1_8, MINECRAFT_1_7_10, MINECRAFT_1_7_5, MINECRAFT_1_6_4, MINECRAFT_1_6_2, MINECRAFT_1_6_1, MINECRAFT_1_5_2, MINECRAFT_1_5_1, MINECRAFT_1_4_7, MINECRAFT_LEGACY, UNKNOWN
+
+You can also check this from [Enum ProtocolVersion](https://github.com/ProtocolSupport/ProtocolSupport/blob/mcpenew/src/protocolsupport/api/ProtocolVersion.java) 
--- src/main/java/tk/itstake/chprotocolsupport/Functions.java
+++ src/main/java/tk/itstake/chprotocolsupport/Functions.java
@@ -2,15 +2,17 @@ package tk.itstake.chprotocolsupport;
 
 import com.laytonsmith.PureUtilities.SimpleVersion;
 import com.laytonsmith.PureUtilities.Version;
-import com.laytonsmith.abstraction.MCCommandSender;
-import com.laytonsmith.abstraction.bukkit.entities.BukkitMCPlayer;
+import com.laytonsmith.abstraction.MCPlayer;
 import com.laytonsmith.annotations.api;
 import com.laytonsmith.core.Static;
+import com.laytonsmith.core.constructs.CArray;
+import com.laytonsmith.core.constructs.CInt;
 import com.laytonsmith.core.constructs.CString;
 import com.laytonsmith.core.constructs.Construct;
 import com.laytonsmith.core.constructs.Target;
-import com.laytonsmith.core.environments.CommandHelperEnvironment;
 import com.laytonsmith.core.environments.Environment;
+import com.laytonsmith.core.exceptions.CRE.CREException;
+import com.laytonsmith.core.exceptions.CRE.CREIllegalArgumentException;
 import com.laytonsmith.core.exceptions.CRE.CREInvalidPluginException;
 import com.laytonsmith.core.exceptions.CRE.CRENotFoundException;
 import com.laytonsmith.core.exceptions.CRE.CREPlayerOfflineException;
@@ -21,6 +23,11 @@ import com.laytonsmith.core.natives.interfaces.Mixed;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import protocolsupport.api.ProtocolSupportAPI;
+import protocolsupport.api.ProtocolVersion;
+
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by bexco on 2016-03-12.
@@ -32,20 +39,22 @@ public class Functions {
     }
 
     private static void checkPlugin(Target t) {
-        if(!Bukkit.getPluginManager().isPluginEnabled("ProtocolSupport")) {
+        if (!Bukkit.getPluginManager().isPluginEnabled("ProtocolSupport")) {
             throw new CREInvalidPluginException("Can't find ProtocolSupport", t);
         }
     }
 
     @api
     public static class get_protocol_version extends AbstractFunction {
+        private static final String PROTOCOL_LIST_STR = buildProtocolListString();
 
         @Override
         public Class<? extends CREThrowable>[] thrown() {
             return new Class[]{
                     CREInvalidPluginException.class,
                     CREPlayerOfflineException.class,
-                    CRENotFoundException.class
+                    CRENotFoundException.class,
+                    CREIllegalArgumentException.class
             };
         }
 
@@ -59,21 +68,51 @@ public class Functions {
             return false;
         }
 
-        @Override
-        public Construct exec(Target t, Environment environment, Mixed... args) throws ConfigRuntimeException {
-            checkPlugin(t);
-            Player p = null;
-            if(args.length == 0) {
-                MCCommandSender m = environment.getEnv(CommandHelperEnvironment.class).GetCommandSender();
-                if (m instanceof BukkitMCPlayer) {
-                    p = ((BukkitMCPlayer) m)._Player();
-                } else {
-                    throw new CREPlayerOfflineException("Player is Offline or Not exist", t);
+        public static String buildProtocolListString() {
+            StringBuilder builder = new StringBuilder();
+            for (ProtocolVersion value : ProtocolVersion.values()) {
+                if (builder.length() > 0) {
+                    builder.append(", ");
                 }
-            } else if(args.length == 1) {
-                p = ((BukkitMCPlayer) Static.GetPlayer(args[0], t))._Player();
+                builder.append(value.name());
             }
-            return new CString(ProtocolSupportAPI.getProtocolVersion(p).getName(), t);
+            return builder.toString();
+        }
+
+        public static ProtocolVersion getProtocolVersion(MCPlayer player, Target t) {
+            Object handle = player.getHandle();
+            if (handle instanceof Player) {
+                return ProtocolSupportAPI.getProtocolVersion((Player) handle);
+            }
+            throw new CREIllegalArgumentException(
+                    String.format("Expected %s but received %s.",
+                            Player.class.getName(), handle.getClass().getName()),
+                    t
+            );
+        }
+
+        public static ProtocolVersion getProtocolVersion(Mixed argument, Target t) {
+            try {
+                return getProtocolVersion(Static.GetPlayer(argument, t), t);
+            } catch (Exception ex) {
+                // Ignore
+            }
+            String query = argument.val();
+            try {
+                return ProtocolVersion.valueOf(query);
+            } catch (IllegalArgumentException ex) {
+                throw new CREIllegalArgumentException("Offline player or unknown protocol name.\n" +
+                        "Available protocol names: " + PROTOCOL_LIST_STR, t, ex);
+            }
+        }
+
+        @Override
+        public Construct exec(Target t, Environment env, Mixed... args) throws ConfigRuntimeException {
+            checkPlugin(t);
+            ProtocolVersion version = args.length >= 1
+                    ? getProtocolVersion(args[0], t)
+                    : getProtocolVersion(Static.getPlayer(env, t), t);
+            return new CString(version.getName(), t);
         }
 
         @Override
@@ -88,7 +127,81 @@ public class Functions {
 
         @Override
         public String docs() {
-            return "{[player]} version Returns a Protocol Version.";
+            return "string {[player|protocolname]} version Returns a Protocol Version.";
+        }
+
+        @Override
+        public Version since() {
+            return new SimpleVersion(1, 0, 0);
+        }
+    }
+
+    @api
+    public static class get_protocol_data extends AbstractFunction {
+        private static final Map<ProtocolVersion, Integer> orderCaches = new HashMap<>();
+
+        public static Integer getOrder(ProtocolVersion version, Target t) {
+            return orderCaches.computeIfAbsent(version, v -> {
+                try {
+                    Field orderIdField = ProtocolVersion.class.getDeclaredField("orderId");
+                    orderIdField.setAccessible(true);
+                    Object orderId = orderIdField.get(version);
+                    Field idField = orderId.getClass().getDeclaredField("id");
+                    idField.setAccessible(true);
+                    return idField.getInt(orderId);
+                } catch (Exception ex) {
+                    throw new CREException("Failed while getting orderId. ProtocolSupport changed?", t, ex);
+                }
+            });
+        }
+
+        @Override
+        public Class<? extends CREThrowable>[] thrown() {
+            return new Class[]{
+                    CREInvalidPluginException.class,
+                    CREPlayerOfflineException.class,
+                    CRENotFoundException.class,
+                    CREIllegalArgumentException.class
+            };
+        }
+
+        @Override
+        public boolean isRestricted() {
+            return false;
+        }
+
+        @Override
+        public Boolean runAsync() {
+            return false;
+        }
+
+        @Override
+        public Construct exec(Target t, Environment env, Mixed... args) throws ConfigRuntimeException {
+            checkPlugin(t);
+            ProtocolVersion version = args.length >= 1
+                    ? get_protocol_version.getProtocolVersion(args[0], t)
+                    : get_protocol_version.getProtocolVersion(Static.getPlayer(env, t), t);
+            CArray array = new CArray(t);
+            array.set("id", new CInt(version.getId(), t), t);
+            array.set("order", new CInt(getOrder(version, t), t), t);
+            array.set("name", new CString(version.getName(), t), t);
+            array.set("type", new CString(version.getProtocolType().name(), t), t);
+            return array;
+        }
+
+        @Override
+        public String getName() {
+            return "get_protocol_data";
+        }
+
+        @Override
+        public Integer[] numArgs() {
+            return new Integer[]{0, 1};
+        }
+
+        @Override
+        public String docs() {
+            return "array {[player|protocolname]} Returns a protocol data.";
         }
 
         @Override
